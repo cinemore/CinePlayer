261017b250c5c17dd2e483f5fa671039351adf30
xzhih <triton2you@gmail.com>
2026-02-25 22:59:34 +0800

    create project

---
+++ CinePlayer/CinePlayerApp.swift
@@ -0,0 +1,32 @@
+//
+//  CinePlayerApp.swift
+//  CinePlayer
+//
+//  Created by Zero on 2026/2/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CinePlayerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
